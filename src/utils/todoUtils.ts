c577e5a00f477bd5c4a0120837ff31d2cb37ce24
Zhykharieva <46319208+Zhykharieva@users.noreply.github.com>
2021-06-15 13:59:56 +0300

    add comment to unused cases

--- src/utils/todoUtils.ts
+++ src/utils/todoUtils.ts
@@ -55,6 +55,7 @@ export const sortItemsList = (todos: Todo[], value: string): Todo[] => {
         return Number(b.impotant) - Number(a.impotant);
       });
       return currentTodos;
+    // We should add these options after the development of the corresponding functions: deadline and day list
     // case SortOptions.creationDate:
     //   break;
     // case SortOptions.myDayList:
