86370185d11965cb52e4a112b3b2c4b8e68e83f1
Dima <ifffweknow@gmail.com>
2021-06-18 14:34:38 +0300

    add sort deadline and change 'impotant' to 'important'

--- src/components/TodoListItem/TodoListItem.tsx
+++ src/components/TodoListItem/TodoListItem.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components/macro";
 import { Todo } from "~src/types";
 import { COLORS } from "~src/colors";
 import { Context } from "~src/context/context";
-import { completedTodo, incompletedTodo, toggleImpotantTodo } from "~src/utils/todoUtils";
+import { toggleCompletedTodo, toggleImportantTodo } from "~src/utils/todoUtils";
 
 const StyledContainer = styled.div`
   display: flex;
@@ -12,7 +12,7 @@ const StyledContainer = styled.div`
   font-size: 1.2rem;
   padding: 1rem 0;
   border-bottom: 1px solid ${COLORS.lightGrey};
-  box-shadow-bottom: inset 0 1px 0 0 ${COLORS.lightGrey};
+  box-shadow: inset 0 1px 0 0 ${COLORS.lightGrey};
   cursor: pointer;
   &:hover {
     background-color: ${COLORS.lightGrey};
@@ -35,37 +35,24 @@ const StyledIcon = styled.i`
   color: ${(props) => props.theme.color};
 `;
 
-export const TodoListItem: React.FC<Todo> = ({ title, completed, impotant, id }) => {
+export const TodoListItem: React.FC<Todo> = ({ title, completed, important, id }) => {
   const { todos, updateTodo } = useContext(Context);
 
-  const onCompletedTodo = useCallback(() => {
-    const newItem = completedTodo(id, todos);
+  const onToggleCompletedTodo = useCallback(() => {
+    const newItem = toggleCompletedTodo(id, todos);
     updateTodo(newItem, id);
   }, [id, todos, updateTodo]);
 
-  const onIncompleteTodo = useCallback(() => {
-    const newItem = incompletedTodo(id, todos);
-    updateTodo(newItem, id);
-  }, [id, todos, updateTodo]);
-
-  const onToggleImpotantTodo = useCallback(() => {
-    const newItem = toggleImpotantTodo(id, todos);
+  const onToggleImportantTodo = useCallback(() => {
+    const newItem = toggleImportantTodo(id, todos);
     updateTodo(newItem, id);
   }, [id, todos, updateTodo]);
 
   return (
     <StyledContainer>
-      {completed ? (
-        <StyledIcon onClick={onIncompleteTodo} className="fa fa-check-circle" />
-      ) : (
-        <StyledIcon onClick={onCompletedTodo} className="fa fa-circle-thin" />
-      )}
+      <StyledIcon onClick={onToggleCompletedTodo} className={completed ? "fa fa-check-circle" : "fa fa-circle-thin"} />
       <StyledButton>{title}</StyledButton>
-      {impotant ? (
-        <StyledIcon onClick={onToggleImpotantTodo} className="fa fa-star" />
-      ) : (
-        <StyledIcon onClick={onToggleImpotantTodo} className="fa fa-star-o" />
-      )}
+      <StyledIcon onClick={onToggleImportantTodo} className={important ? "fa fa-star" : "fa fa-star-o"} />
     </StyledContainer>
   );
 };
--- src/utils/todoUtils.ts
+++ src/utils/todoUtils.ts
@@ -6,11 +6,12 @@ export const createTodoItem = (title: string, todoType: string): Todo => {
     id: Date.now(),
     date: Date.now(),
     title,
-    impotant: false,
+    important: false,
     completed: false,
     myDay: false,
     planned: false,
     assigned: false,
+    timeCompleted: 0,
     [todoType]: true,
   };
 };
@@ -25,22 +26,16 @@ const findInd = (id: number, todos: Todo[]): number => {
   return todos.findIndex((todo) => todo.id === id);
 };
 
-export const completedTodo = (id: number, todos: Todo[]): Todo => {
+export const toggleCompletedTodo = (id: number, todos: Todo[]): Todo => {
   const ind = findInd(id, todos);
   const oldItem = todos[ind];
-  return { ...oldItem, completed: true };
+  return { ...oldItem, completed: !oldItem.completed, timeCompleted: +`${oldItem.timeCompleted ? 0 : Date.now()}` };
 };
 
-export const incompletedTodo = (id: number, todos: Todo[]): Todo => {
+export const toggleImportantTodo = (id: number, todos: Todo[]): Todo => {
   const ind = findInd(id, todos);
   const oldItem = todos[ind];
-  return { ...oldItem, completed: false };
-};
-
-export const toggleImpotantTodo = (id: number, todos: Todo[]): Todo => {
-  const ind = findInd(id, todos);
-  const oldItem = todos[ind];
-  return { ...oldItem, impotant: !oldItem.impotant };
+  return { ...oldItem, important: !oldItem.important };
 };
 
 export const update = (todos: Todo[], newItem: Todo, id: number): Todo[] => {
@@ -56,15 +51,13 @@ export const sortItemsList = (todos: Todo[], value: string): Todo[] => {
   const currentTodos = todos.slice();
   switch (value) {
     case SortOptions.importance:
-      currentTodos.sort((a, b) => {
-        return Number(b.impotant) - Number(a.impotant);
-      });
+      currentTodos.sort((a, b) => Number(a.important) - Number(b.important));
       return currentTodos;
-    // We should add these options after the development of the corresponding next functions: deadline and day list
-    // case SortOptions.creationDate:
-    //   break;
-    // case SortOptions.myDayList:
-    //   break;
+
+    case SortOptions.deadline:
+      currentTodos.sort((a, b) => a.timeCompleted - b.timeCompleted);
+      return currentTodos;
+
     case SortOptions.alphabet:
       currentTodos.sort((a, b) => a.title.localeCompare(b.title));
       return currentTodos;
